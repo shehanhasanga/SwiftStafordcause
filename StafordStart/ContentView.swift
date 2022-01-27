f45a7643e7f0da536cb662ec2081936e70ef2f4c
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-01-27 10:06:57 +0530

    first commit

--- StafordStart/ContentView.swift
+++ StafordStart/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding()
+        .foregroundColor(Color.red)
+        
+       
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct CardView: View {
+    var faceUp:Bool = false
+    var body: some View {
+       
+        return
+        ZStack{
+            if(faceUp){
+                RoundedRectangle(cornerRadius: 25)
+                    .fill()
+                RoundedRectangle(cornerRadius: 25)
+                    .stroke(lineWidth: 3)
+                Text("Hello, world!")
+                    .padding(.vertical)
+            } else {
+                RoundedRectangle(cornerRadius: 25)
+                    .stroke(lineWidth: 3)
+                Text("Hello, world!")
+                    .padding(.vertical)
+            }
+            
+        }
+       
+        
+       
     }
 }

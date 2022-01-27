ac1650d80e34f433a6958b22617b94597a557902
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-01-27 08:42:45 +0530

    Initial Commit

---
+++ StafordStart/StafordStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  StafordStartApp.swift
+//  StafordStart
+//
+//  Created by shehan karunarathna on 2022-01-27.
+//
+
+import SwiftUI
+
+@main
+struct StafordStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

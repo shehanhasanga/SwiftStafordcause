1ec6136575d6d871875f725d3b4a9f8f7d25b767
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-01-27 11:33:27 +0530

    added section 2 changes

--- StafordStart/ContentView.swift
+++ StafordStart/ContentView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojies = ["🚙","🚁","🚀","🛶","🚘","⛵️","🛳","🚌","🛻","🛴","🛺","🚑","🚜","🚒","🛸"]
+    @State var emojiCount = 5
+    var remove : some View{
+        return  Button {
+            if(emojiCount > 1){
+                emojiCount-=1
+            }
+           
+        } label: {
+            VStack{
+                Image(systemName: "minus.circle")
+            }
+        }
+    }
+    var add : some View{
+        Button {
+            if(emojiCount < emojies.count){
+                emojiCount+=1
+            }
+            
+        } label: {
+            VStack{
+                Image(systemName: "plus.circle")
+            }
+        }
+    }
+    
     var body: some View {
         return
-        HStack{
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        
+        VStack{
+            ScrollView{
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: 70))]){
+                    ForEach(emojies[1..<emojiCount], id: \.self) { emoji in
+                        CardView(text: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+
+                }
+                .padding(.all)
+            }
+            .foregroundColor(Color.red)
+            Spacer(minLength: 20)
+            HStack{
+                remove
+                Spacer()
+                add
+                
+                
+            }
+            .padding(.horizontal)
+            
+
         }
+        .font(.largeTitle)
         .padding()
-        .foregroundColor(Color.red)
+        
         
        
     }
@@ -31,28 +77,25 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct CardView: View {
-    var faceUp:Bool = false
+    @State var faceUp:Bool = true
+    var text:String
     var body: some View {
-       
         return
         ZStack{
+            let baseShape = RoundedRectangle(cornerRadius: 25)
             if(faceUp){
-                RoundedRectangle(cornerRadius: 25)
-                    .fill()
-                RoundedRectangle(cornerRadius: 25)
-                    .stroke(lineWidth: 3)
-                Text("Hello, world!")
+                baseShape.fill(.white)
+                baseShape.stroke(lineWidth: 3)
+                Text(text)
                     .padding(.vertical)
             } else {
-                RoundedRectangle(cornerRadius: 25)
-                    .stroke(lineWidth: 3)
-                Text("Hello, world!")
-                    .padding(.vertical)
+                baseShape.fill()
             }
             
         }
-       
-        
-       
+        .onTapGesture {
+            faceUp = !faceUp
+        }
     }
+    
 }
